6f16024329bb9288cafef9f3ddc459731249aecd
mgr-cse <maitreygovind@gmail.com>
2023-08-01 12:43:46 +0530

    added kubernetes port binding address

--- milestone-3/port-forward.sh
+++ milestone-3/port-forward.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # expose frontend
-kubectl --namespace=inventory-space port-forward inventory 8081:8081 &
+kubectl --namespace=inventory-space port-forward inventory 8081:8081 --address=0.0.0.0 &
 
 # expose read api
-kubectl  --namespace=inventory-space port-forward inventory 8080:8080 &
+kubectl  --namespace=inventory-space port-forward inventory 8080:8080 --address=0.0.0.0 &
 
 # expose mqtt port
-kubectl  --namespace=inventory-space port-forward inventory 9002:9002 &
+kubectl  --namespace=inventory-space port-forward inventory 9002:9002 --address=0.0.0.0 &
 
 wait
